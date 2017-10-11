1d5007d988c1961f6032ef68cfe1b25768bc928d
MDRRC <EVERS4202000@YAHOO.COM>
2017-10-11 19:55:38 +0200

    Formatting updated.

--- Loclib.h
+++ Loclib.h
@@ -10,163 +10,175 @@
 
 #include <Arduino.h>
 
-class LocLib {
- public:
-  /**
-   * Decoder steps of a loc.
-   */
-  enum decoderSteps { decoderStep14 = 0, decoderStep28, decoderStep128 };
+class LocLib
+{
+public:
+    /**
+     * Decoder steps of a loc.
+     */
+    enum decoderSteps
+    {
+        decoderStep14 = 0,
+        decoderStep28,
+        decoderStep128
+    };
 
-  /**
-   * Direction of a loc.
-   */
-  enum direction { directionForward = 0, directionBackWard };
+    /**
+     * Direction of a loc.
+     */
+    enum direction
+    {
+        directionForward = 0,
+        directionBackWard
+    };
 
-  /**
-   * Function options.
-   */
-  enum function {
-    functionOff = 0,
-    functionOn,
-    functionToggle,
-    functionNone,
-  };
+    /**
+     * Function options.
+     */
+    enum function
+    {
+        functionOff = 0,
+        functionOn,
+        functionToggle,
+        functionNone,
+    };
 
-  /**
-   * Actual loc data of selected loc.
-   */
-  struct LocLibData {
-    uint16_t Addres;               /* Address of loc */
-    uint16_t Speed;                /* Actual speed of loc */
-    direction Dir;                 /* Direction of loc */
-    decoderSteps Steps;            /* Decoder steps of loc */
-    uint32_t Function;             /* Actual functions of loc. */
-    uint8_t FunctionAssignment[5]; /* Assigned functions to buttons of loc. */
-  };
+    /**
+     * Actual loc data of selected loc.
+     */
+    struct LocLibData
+    {
+        uint16_t Addres;               /* Address of loc */
+        uint16_t Speed;                /* Actual speed of loc */
+        direction Dir;                 /* Direction of loc */
+        decoderSteps Steps;            /* Decoder steps of loc */
+        uint32_t Function;             /* Actual functions of loc. */
+        uint8_t FunctionAssignment[5]; /* Assigned functions to buttons of loc. */
+    };
 
-  /* Constructor. */
-  LocLib();
+    /* Constructor. */
+    LocLib();
 
-  /**
-   * Init of loc module.
-   */
-  void Init();
+    /**
+     * Init of loc module.
+     */
+    void Init();
 
-  /**
-   * Get pointer to data of selected loc.
-   */
-  LocLibData* DataGet();
+    /**
+     * Get pointer to data of selected loc.
+     */
+    LocLibData* DataGet();
 
-  /**
-   * Increase, decrease, stop or reverse direction of selected loc.
-   */
-  bool SpeedSet(int8_t Delta);
+    /**
+     * Increase, decrease, stop or reverse direction of selected loc.
+     */
+    bool SpeedSet(int8_t Delta);
 
-  /**
-   * Get actual speed of selected loc.
-   */
-  uint8_t SpeedGet(void);
+    /**
+     * Get actual speed of selected loc.
+     */
+    uint8_t SpeedGet(void);
 
-  /**
-   * Write direct actual speed of selected loc.
-   */
-  void SpeedUpdate(uint8_t Speed);
+    /**
+     * Write direct actual speed of selected loc.
+     */
+    void SpeedUpdate(uint8_t Speed);
 
-  /**
-   * Update decoder type of selected loc.
-   */
-  void DecoderStepsUpdate(decoderSteps Steps);
+    /**
+     * Update decoder type of selected loc.
+     */
+    void DecoderStepsUpdate(decoderSteps Steps);
 
-  /**
-   * Get actual decoder steps of selected loc,
-   */
-  decoderSteps DecoderStepsGet(void);
+    /**
+     * Get actual decoder steps of selected loc,
+     */
+    decoderSteps DecoderStepsGet(void);
 
-  /**
-   * Toggle direction of selected loc.
-   */
-  void DirectionToggle(void);
+    /**
+     * Toggle direction of selected loc.
+     */
+    void DirectionToggle(void);
 
-  /**
-   * Get actual direction of selected loc,
-   */
-  direction DirectionGet(void);
+    /**
+     * Get actual direction of selected loc,
+     */
+    direction DirectionGet(void);
 
-  /**
-   * Set direction of actual selected loc.
-   */
-  void DirectionSet(direction dir);
+    /**
+     * Set direction of actual selected loc.
+     */
+    void DirectionSet(direction dir);
 
-  /**
-   * Write direct function data for selected loc.
-   */
-  void FunctionUpdate(uint32_t FunctionData);
+    /**
+     * Write direct function data for selected loc.
+     */
+    void FunctionUpdate(uint32_t FunctionData);
 
-  /**
-   * Toggle selected function of selected loc.
-   */
-  void FunctionToggle(uint8_t number);
+    /**
+     * Toggle selected function of selected loc.
+     */
+    void FunctionToggle(uint8_t number);
 
-  /**
-   * Get the assigned function to a button.
-   */
-  uint8_t FunctionAssignedGet(uint8_t number);
+    /**
+     * Get the assigned function to a button.
+     */
+    uint8_t FunctionAssignedGet(uint8_t number);
 
-  /**
-   * Get the status of a function.
-   */
-  function FunctionStatusGet(uint32_t number);
+    /**
+     * Get the status of a function.
+     */
+    function FunctionStatusGet(uint32_t number);
 
-  /**
-   * Get the next or previous loc from stored in EEPROM.
-   */
-  uint16_t GetNextLoc(int8_t Delta);
+    /**
+     * Get the next or previous loc from stored in EEPROM.
+     */
+    uint16_t GetNextLoc(int8_t Delta);
 
-  /**
-   * Get address of actual selected loc.
-   */
-  uint16_t GetActualLocAddress(void);
+    /**
+     * Get address of actual selected loc.
+     */
+    uint16_t GetActualLocAddress(void);
 
-  /**
-   * Check if loc is present in EEPROM.
-   */
-  uint8_t CheckLoc(uint16_t address);
+    /**
+     * Check if loc is present in EEPROM.
+     */
+    uint8_t CheckLoc(uint16_t address);
 
-  /**
-   * Store locomotive in EEPROM.
-   */
-  bool StoreLoc(uint16_t address, uint8_t* FunctionAssigment);
+    /**
+     * Store locomotive in EEPROM.
+     */
+    bool StoreLoc(uint16_t address, uint8_t* FunctionAssigment);
 
-  /**
-   * Remove loc from EEPROM.
-   */
-  bool RemoveLoc(uint16_t address);
+    /**
+     * Remove loc from EEPROM.
+     */
+    bool RemoveLoc(uint16_t address);
 
-  /**
-   * Get actual number of locs in EEPROM.
-   */
-  uint16_t GetNumberOfLocs(void);
+    /**
+     * Get actual number of locs in EEPROM.
+     */
+    uint16_t GetNumberOfLocs(void);
 
-  /**
-   * Get the index of the selected loc.
-   */
-  uint16_t GetActualSelectedLocIndex(void);
+    /**
+     * Get the index of the selected loc.
+     */
+    uint16_t GetActualSelectedLocIndex(void);
 
-  /**
-   * Sort loc data in EEPROM.
-   */
-  void LocBubbleSort(void);
+    /**
+     * Sort loc data in EEPROM.
+     */
+    void LocBubbleSort(void);
 
- private:
-  LocLibData m_LocLibData;     /* Data of actual selected loc. */
-  uint8_t m_NumberOfLocs;      /* Number of locs. */
-  uint8_t m_ActualSelectedLoc; /* Actual selected loc. */
+private:
+    LocLibData m_LocLibData;     /* Data of actual selected loc. */
+    uint8_t m_NumberOfLocs;      /* Number of locs. */
+    uint8_t m_ActualSelectedLoc; /* Actual selected loc. */
 
-  const uint8_t locLibEepromVersion = 2; /* Version of data in EEPROM. */
-  const int locLibEepromAddressData = 2; /* Start in EEPROM address loc data. */
-  const uint8_t locLibMaxNumberOfLocs = 64; /* Max number of locs in EEPROM. */
-  const int locLibEepromAddressVersion = 0; /* EEPROM address version info. */
-  const int locLibEepromAddressNumOfLocs = 1; /* EEPROM address num of locs. */
+    const uint8_t locLibEepromVersion      = 2;  /* Version of data in EEPROM. */
+    const int locLibEepromAddressData      = 2;  /* Start in EEPROM address loc data. */
+    const uint8_t locLibMaxNumberOfLocs    = 64; /* Max number of locs in EEPROM. */
+    const int locLibEepromAddressVersion   = 0;  /* EEPROM address version info. */
+    const int locLibEepromAddressNumOfLocs = 1;  /* EEPROM address num of locs. */
 };
 
 #endif
