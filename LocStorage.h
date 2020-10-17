db4dc543c3f566f4dd045bfcf209cb85597a2a05
Dirk <hoffmann@vmd-jena.de>
2020-10-17 02:03:51 +0200

    Removed adc button related stuff

--- LocStorage.h
+++ LocStorage.h
@@ -72,9 +72,6 @@ public:
     void SelectedLocIndexStore(uint8_t Index);
     uint8_t SelectedLocIndexGet();
     void EraseEeprom(void);
-#if APP_CFG_UC == APP_CFG_UC_ESP8266
-    void InvalidateAdc();
-#endif
 
 private:
 #if APP_CFG_UC == APP_CFG_UC_STM32
